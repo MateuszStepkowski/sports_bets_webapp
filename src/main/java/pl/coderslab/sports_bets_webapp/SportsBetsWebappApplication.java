fc32a4589c7fc643e82330c38962e38312f95394
MateuszStępkowski <stepkowski.mat@gmail.com>
2018-09-18 16:29:00 +0200

    init commit --> project with AppInitializer created, basic application.properties for JMS and DB filled

---
+++ src/main/java/pl/coderslab/sports_bets_webapp/SportsBetsWebappApplication.java
@@ -0,0 +1,12 @@
+package pl.coderslab.sports_bets_webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsBetsWebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportsBetsWebappApplication.class, args);
+    }
+}
---
+++ src/test/java/pl/coderslab/sports_bets_webapp/SportsBetsWebappApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.coderslab.sports_bets_webapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SportsBetsWebappApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
